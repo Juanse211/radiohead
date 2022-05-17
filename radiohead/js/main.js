e980b68ba0cae68e2d777dd40694d92f8af3ce4d
Juan Albornoz <juansexy211@gmail.com>
2022-05-17 16:18:24 -0300

    fix error

--- radiohead/js/main.js
+++ radiohead/js/main.js
@@ -1,6 +1,29 @@
-const btnSwitch = document.querySelector('#button__body');
+const btnSwitch = document.querySelector('#switch__body');
 
 btnSwitch.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   btnSwitch.classList.toggle('active');
+
+// Save mode in local storage
+  if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode', 'true');
+  } else{
+		localStorage.setItem('dark-mode', 'false');
+  }
 });
+
+//
+if(localStorage.getItem('dark-mode') === 'true'){
+  document.body.classList.add('dark');
+} else{
+  document.body.classList.remove('dark');
+}
+
+// Save switch condition.
+if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active');
+} else {
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active');
+} 
